ab49f4cf51f15ab57ec4fe0f2ed70dba4528b1f7
xmoure <ximenamoure@gmail.com>
2023-10-17 20:04:37 +0200

    pylint fixes

--- src/models/train.py
+++ src/models/train.py
@@ -1,6 +1,7 @@
-import mlflow
+"""Module that trains the model """
 import os
 from getpass import getpass
+import mlflow
 from ultralytics import YOLO
 import yaml
 
@@ -11,7 +12,7 @@ os.environ['MLFLOW_TRACKING_URI'] = input('Enter your DAGsHub project tracking U
 
 mlflow.set_tracking_uri(os.environ['MLFLOW_TRACKING_URI'])
 
-with open(r"params.yaml") as f:
+with open(r"params.yaml", encoding='utf-8') as f:
     params = yaml.safe_load(f)
 
 # Load the model.
@@ -20,10 +21,9 @@ model = YOLO(params['model_type'])
 # Training.
 mlflow.set_experiment(params['name'])
 with mlflow.start_run(run_name=params['name']):
-  results = model.train(
-    data='../../data/yolov8_format/data.yaml',
-    imgsz=params['imgsz'],
-    epochs=params['epochs'],
-    batch=params['batch'],
-    name=params['name'])
-
+    results = model.train(
+      data='../../data/yolov8_format/data.yaml',
+      imgsz=params['imgsz'],
+      epochs=params['epochs'],
+      batch=params['batch'],
+      name=params['name'])
