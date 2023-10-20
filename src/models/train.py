95ba39d32b4bc7c06b1b03b2b3135597c00970f7
xangeee <ange.xu@estudiantat.upc.edu>
2023-10-20 18:27:30 +0200

    modified  tests

--- src/models/train.py
+++ src/models/train.py
@@ -11,10 +11,8 @@ import pandas as pd
 from src import METRICS_DIR,MODELS_DIR,ROOT_DIR,DATA_YAML_DIR,ARTIFACTS_DIR,REPORTS_DIR
 import shutil
 
-# os.environ['MLFLOW_TRACKING_USERNAME'] = input('Enter your DAGsHub username: ')
-# os.environ['MLFLOW_TRACKING_PASSWORD'] = input('Enter your DAGsHub access token: ')
-os.environ['MLFLOW_TRACKING_PASSWORD'] = "7b8ed6a4b1ca4e491be311356a32bbb13ade8bb5"
-os.environ['MLFLOW_TRACKING_USERNAME'] = "xangeee"
+os.environ['MLFLOW_TRACKING_USERNAME'] = input('Enter your DAGsHub username: ')
+os.environ['MLFLOW_TRACKING_PASSWORD'] = input('Enter your DAGsHub access token: ')
 os.environ['MLFLOW_TRACKING_URI'] = "https://dagshub.com/Sebastianpaglia/MLOps_WhereIsWally.mlflow"
 
 mlflow.set_tracking_uri(os.environ['MLFLOW_TRACKING_URI'])
@@ -30,25 +28,25 @@ model = YOLO(params['model_type'])
 # Training.
 mlflow.set_experiment(params['name'])
 with mlflow.start_run():
-    # with EmissionsTracker(
-    #         output_dir=EMISSIONS_OUTPUT_FOLDER,
-    #         output_file="emissions.csv",
-    #         on_csv_write="update",
-    # ):
-    results = model.train(
-        data=DATA_YAML_DIR,
-        imgsz=params['imgsz'],
-        epochs=params['epochs'],
-        batch=params['batch'],
-        name=params['name']
-        ),
+    with EmissionsTracker(
+            output_dir=EMISSIONS_OUTPUT_FOLDER,
+            output_file="emissions.csv",
+            on_csv_write="update",
+    ):
+        results = model.train(
+            data=DATA_YAML_DIR,
+            imgsz=params['imgsz'],
+            epochs=params['epochs'],
+            batch=params['batch'],
+            name=params['name']
+            ),
 
     # Log the CO2 emissions to MLflow
-    # emissions = pd.read_csv(EMISSIONS_OUTPUT_FOLDER / "emissions.csv")
-    # emissions_metrics = emissions.iloc[-1, 4:13].to_dict()
-    # emissions_params = emissions.iloc[-1, 13:].to_dict()
-    # mlflow.log_params(emissions_params)
-    # mlflow.log_metrics(emissions_metrics)
+    emissions = pd.read_csv(EMISSIONS_OUTPUT_FOLDER / "emissions.csv")
+    emissions_metrics = emissions.iloc[-1, 4:13].to_dict()
+    emissions_params = emissions.iloc[-1, 13:].to_dict()
+    mlflow.log_params(emissions_params)
+    mlflow.log_metrics(emissions_metrics)
     
     # Save the model as a pickle file
     Path("models").mkdir(exist_ok=True)
