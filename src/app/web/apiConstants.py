d9bc2209aecd745d944d0c0e7e94483aa3dbe707
xangeee <ange.xu@estudiantat.upc.edu>
2023-11-15 18:16:24 +0100

    first web application version

---
+++ src/app/web/apiConstants.py
@@ -0,0 +1,4 @@
+
+API_ROOT_URL="http://localhost:5000/"
+MODEL_TYPE="all"
+PREDICT=API_ROOT_URL+"predict/"+ MODEL_TYPE
---
+++ src/app/web/pages/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import requests
+import apiConstants as api
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import render
+import base64
+# Create your views here.
+# pages/views.py
+
+@csrf_exempt
+def upload_image(request):
+    if request.method == 'POST' and request.FILES.get('image'):
+        image = request.FILES['image']
+        encoded_image = base64.b64encode(image.read()).decode('utf-8')
+
+        # API call
+        # TODO: not working the endpoint call 
+        params={'file':encoded_image}
+        response = requests.post(api.PREDICT,params=params)
+        
+        #convert reponse data into json
+        response = response.json()
+        print("API response:",response)
+        
+        return JsonResponse({'status': 'success'})
+
+    return JsonResponse({'status': 'error'})
+
+
+def home(request):
+    return render(request, "pages/home.html", {})
