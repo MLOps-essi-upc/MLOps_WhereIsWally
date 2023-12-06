0d316d6995f99cac0a70642a446d36a7043b97a2
root <root@ivysaur.fib.upc.es>
2023-12-06 20:19:08 +0100

    adding grafana and prometheus to docker-compose

--- src/app/backend/api.py
+++ src/app/backend/api.py
@@ -9,42 +9,99 @@ from fastapi import FastAPI, HTTPException, Request, File, UploadFile,Response
 from ultralytics.utils.plotting import Annotator
 import numpy as np
 import cv2
-from src import MODELS_DIR, API_DIR
+from src import MODELS_DIR
 from ultralytics import YOLO
 import os
+import asyncio
+from prometheus_fastapi_instrumentator import Instrumentator, metrics
 model_wrappers_list: List[dict] = []
 
 # Define application
 app = FastAPI(
     title="Where is Wally",
-    description="lorem ipsum",
+    description="Upload an image and we will help you to find Wally",
     version="0.1",
 )
 
+instrumentator = Instrumentator(
+    should_group_status_codes=False,
+    should_ignore_untemplated=True,
+    should_respect_env_var=True,
+    should_instrument_requests_inprogress=True,
+    excluded_handlers=[".*admin.*", "/metrics"],
+    env_var_name="ENABLE_METRICS",
+    inprogress_name="inprogress",
+    inprogress_labels=True,
+)
+
+instrumentator.add(
+    metrics.request_size(
+        should_include_handler=True,
+        should_include_method=False,
+        should_include_status=True,
+        metric_namespace="a",
+        metric_subsystem="b",
+    )
+).add(
+    metrics.response_size(
+        should_include_handler=True,
+        should_include_method=False,
+        should_include_status=True,
+        metric_namespace="namespace",
+        metric_subsystem="subsystem",
+    )
+)
+
+instrumentator.instrument(app)
+
+instrumentator.expose(app, include_in_schema=False, should_gzip=True)
 
 def construct_response(f):
-    """Construct a JSON response for an endpoint's results."""
-
     @wraps(f)
-    def wrap(request: Request, *args, **kwargs):
-        results = f(request, *args, **kwargs)
+    async def wrap(request: Request, *args, **kwargs):
+        try:
+            if asyncio.iscoroutinefunction(f):
+                results = await f(request, *args, **kwargs)
+            else:
+                results = f(request, *args, **kwargs)
 
-        # Construct response
-        response = {
-            "message": results["message"],
-            "method": request.method,
-            "status-code": results["status-code"],
-            "timestamp": datetime.now().isoformat(),
-            "url": request.url._url,
-        }
+            # Default status code
+            status_code = results.get("status-code", HTTPStatus.OK)
 
-        # Add data
-        if "data" in results:
-            response["data"] = results["data"]
+            response = {
+                "message": results.get("message", status_code.phrase),
+                "method": request.method,
+                "status-code": status_code,
+                "timestamp": datetime.now().isoformat(),
+                "url": request.url._url,
+                "data": results.get("data", {}),
+                "found": results.get("found", None)
+            }
 
-        return response
+            # Include additional keys if present
+            for key in ['boxes', 'conf', 'encoded_img']:
+                if key in results:
+                    response[key] = results[key]
+
+            return response
+
+        except HTTPException as http_exc:
+            # Forward HTTP exceptions as they are
+            raise http_exc
+
+        except Exception as exc:
+            # Handle other exceptions
+            return {
+                "message": "An error occurred",
+                "method": request.method,
+                "status-code": HTTPStatus.INTERNAL_SERVER_ERROR,
+                "timestamp": datetime.now().isoformat(),
+                "url": request.url._url,
+                "detail": str(exc),
+            }
 
     return wrap
+
 
 
 @app.on_event("startup")
@@ -61,7 +118,6 @@ def _load_models():
         with open(path, "rb") as file:
             # model_wrapper = pickle.load(file)
             # model_wrappers_list.append(model_wrapper)
-            print("file",str(file))
             model_wrapper=dict()
             model = YOLO(path)
             model_wrapper["model"]=model
@@ -78,7 +134,7 @@ def _index(request: Request):
     response = {
         "message": HTTPStatus.OK.phrase,
         "status-code": HTTPStatus.OK,
-        "data": {"message": "Hello world"},
+        "data": {"message": "Welcome to Where is Wally!"},
     }
     return response
     
@@ -109,42 +165,51 @@ def _get_models_list(request: Request, type: str = None):
         }
 
 
-
 @construct_response
 @app.post("/predict/{type}")
 async def _predict(type: str,file: UploadFile = File(...)):
     model_wrapper = next((m for m in model_wrappers_list if m["type"] == type), None)
 
-    if model_wrapper:
-        model=model_wrapper['model']
-        contents = file.file.read()
-        # contents = await file.read()
-        nparr = np.fromstring(contents, np.uint8)
-        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-        results = model.predict(source=img, conf=0.25)
-        boxes=results[0].boxes.xyxy
-        conf=results[0].boxes.conf
-        
-        for r in results:
-            annotator = Annotator(img)
-            boxes = r.boxes
-            for box in boxes:
-                b = box.xyxy[0]  # get box coordinates in (top, left, bottom, right) format
-                c = box.cls
-                annotator.box_label(b, model.names[int(c)])
-          
-        return_img = annotator.result()  
-
-        # line that fixed it
-        _, encoded_img = cv2.imencode('.PNG', return_img)
-        encoded_img = base64.b64encode(encoded_img)
-        
+    if not model_wrapper:
+        raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail="Model not found")
+    
     else:
-        raise HTTPException(
-            status_code=HTTPStatus.BAD_REQUEST, detail="Model not found"
-        )
-    return{
-        'boxes':boxes,
-        'conf':conf,
-        'encoded_img': encoded_img,
-    }
+        model = model_wrapper['model']
+        contents = await file.read()
+        nparr = np.frombuffer(contents, np.uint8)
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        if img is None:
+            raise HTTPException(status_code=HTTPStatus.BAD_REQUEST, detail="Invalid image file")
+        else: 
+            results = model.predict(source=img, conf=0.25)
+            boxes = results[0].boxes.xyxy
+            conf = results[0].boxes.conf
+
+            for r in results:
+                annotator = Annotator(img)
+                boxes = r.boxes
+                for box in boxes:
+                    b = box.xyxy[0]  # get box coordinates in (top, left, bottom, right) format
+                    c = box.cls
+                    color = (0, 0, 0)
+                    annotator.box_label(b, model.names[int(c)], color=color)
+
+            return_img = annotator.result()
+
+            _, encoded_img = cv2.imencode('.PNG', return_img)
+            encoded_img = base64.b64encode(encoded_img)
+            is_empty = len(boxes) == 0
+            if(is_empty):
+                return {
+                    'boxes': boxes,
+                    'encoded_img': encoded_img.decode(),
+                    'message': "Processing completed, but Wally was not found in the image.",
+                    'found': False,
+                }
+            
+            return {
+                'boxes': boxes,
+                'conf': conf,
+                'encoded_img': encoded_img.decode(),
+                'found': True
+            }
---
+++ tests/api_test/test_api.py
@@ -0,0 +1,74 @@
+import os
+import pytest
+from fastapi.testclient import TestClient
+from src.app.backend.api import app
+from http import HTTPStatus
+
+
+@pytest.fixture(scope="module", autouse=True)
+def client():
+    with TestClient(app) as client:
+        return client
+    
+
+def test_get_main(client):
+    response = client.get("/")
+    assert response.status_code == HTTPStatus.OK.value
+    response_body = response.json()
+    assert "message" in response_body
+    assert "data" in response_body
+    assert "message" in response_body["data"]
+    assert response_body["message"] == HTTPStatus.OK.phrase
+    assert response_body["data"]["message"] == "Welcome to Where is Wally!"
+
+
+def test_predict_with_invalid_file(client):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current_dir, 'testing_file.txt')
+    with open(file_path, 'rb') as file:
+        response = client.post("/predict/all", files={"file": file})
+    assert response.status_code == 400
+    assert "Invalid image file" in response.text
+
+
+def test_predict_with_valid_image_wally_found_model_all(client):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current_dir, 'testing_img.png')
+    with open(file_path, 'rb') as file:
+        response = client.post("/predict/all", files={"file": file})
+    assert response.status_code == 200
+    response_body = response.json()
+    assert response_body['found'] == True
+    assert "boxes" in  response_body
+    assert 'orig_shape' in response_body['boxes']
+    assert len(response_body['boxes']['orig_shape']) > 0
+
+
+def test_predict_with_valid_image_wally_not_found(client):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current_dir, 'testing_not_found_img.jpeg')
+    with open(file_path, 'rb') as file:
+        response = client.post("/predict/all", files={"file": file})
+    assert response.status_code == 200
+    response_body = response.json()
+    assert "boxes" in  response_body
+    assert "found" in  response_body
+    assert response_body['found'] == False
+    assert "Processing completed, but Wally was not found in the image." in response.text
+    assert len(response_body['boxes']) == 0
+
+
+
+def test_predict_with_valid_image_model_wally(client):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    file_path = os.path.join(current_dir, 'testing_img.png')
+    with open(file_path, 'rb') as file:
+        response = client.post("/predict/wally", files={"file": file})
+    assert response.status_code == 200
+    response_body = response.json()
+    assert response_body['found'] == True
+    assert "boxes" in  response_body
+    assert 'orig_shape' in response_body['boxes']
+    assert len(response_body['boxes']['orig_shape']) > 0
+
+
